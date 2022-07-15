f18b854df02dbfd8668ec9741ab645a48f997cbd
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-16 02:01:53 +0800

    😅📩 -> New project, old one archived

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const STUDIO_REWRITE = {
-  source: '/studio/:path*',
-  destination:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3333/studio/:path*'
-      : '/studio/index.html',
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = {
-  rewrites: () => [STUDIO_REWRITE],
-}
+const nextConfiguration = {
+  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+};
+
+module.exports = withPlugins([optimizedImages], nextConfiguration);
